5877503cdc97ad78fddde0180ca0c3fa2bf04a4e
Rabben82 <89846052+Rabben82@users.noreply.github.com>
2023-09-14 15:26:07 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Exercise2
+{
+    internal class Program
+    {
+        private const string menuQuit = "0";
+        private static string userInput = string.Empty;
+        static void Main(string[] args)
+        {
+            bool isTrue = false;
+
+            
+            do
+            {
+                Menu();
+
+                userInput = Console.ReadLine()!;
+
+                switch (userInput)
+                {
+                    case menuQuit:
+                        isTrue = true;
+                        Console.WriteLine("Goodbye :)");
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Wrong input, press any key to try again!");
+                        Console.ReadKey();
+                        break;
+                }
+
+            } while (!isTrue);
+        }
+
+        private static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Welcome to the main menu!\n");
+            Console.WriteLine("To make a selection, enter the number corresponding to your choice:");
+            Console.WriteLine("0. Quit!");
+        }
+    }
+}
